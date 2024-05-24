f7ec42385748181da5ff8f6a413ee70be800121a
Ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-05-24 12:02:50 +0200

    first_commit

---
+++ src/main/java/com/project1/App.java
@@ -0,0 +1,53 @@
+package com.project1;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        for (byte isNotMeter = 0; isNotMeter == 0;) {
+            System.out.print("Escribe tu peso (kg) ");
+            Scanner scanner = new Scanner(System.in);
+            float numberWeight = scanner.nextFloat();
+
+            System.out.print("Escribe tu tamaño en metros (m) ");
+            float numberHeight = scanner.nextFloat();
+            if (numberHeight < 3) {
+                isNotMeter +=1;
+                byte number = 2;
+                float sumNumberHeightX2 = numberHeight * number;
+                float sumWightHeight =  numberWeight / sumNumberHeightX2;
+                DecimalFormat decimalShort = new DecimalFormat("#.##");
+                double resultShort = Double.valueOf(decimalShort.format(sumWightHeight));
+                if(sumWightHeight <= 16) {
+                    System.out.println("Usted tiene " + resultShort + ". Hasta 16 delgadez severa");
+                }
+                if(sumWightHeight >= 16 && sumWightHeight < 17) {
+                    System.out.println("Usted tiene " + resultShort + ". Desde 16 hasta 17 " + "delgadez moderada");
+                }
+                if(sumWightHeight >= 17 && sumWightHeight < 18.5) {
+                    System.out.println("Usted tiene " + resultShort + ". Desde 17 hasta 18 " + "delgadez leve");
+                }
+                if(sumWightHeight >= 18.5 && sumWightHeight < 25) {
+                    System.out.println("Usted tiene " + resultShort + ". Desde 18.5 hasta 25 " + "peso normal");
+                }
+                if(sumWightHeight >= 25 && sumWightHeight < 30) {
+                    System.out.println("Usted tiene " + resultShort + ". Desde 25 hasta 30 " + "sobrepeso");
+                }
+                if(sumWightHeight >= 30 && sumWightHeight < 35) {
+                    System.out.println("Usted tiene " + resultShort + ". Desde 30 hasta 35 " + "obesidad leve");
+                }
+                if(sumWightHeight >= 35 && sumWightHeight < 40) {
+                    System.out.println("Usted tiene " + resultShort + ". Desde 35 hasta 40 " + "obesidad moderada");
+                }
+                if(sumWightHeight >= 40) {
+                    System.out.println("Usted tiene " + resultShort + ". Hasta 40 " + "obesidad morbida");
+                }
+            } else {
+                System.out.println("Un error, prueba escribir el tamaño en metros.");
+            }
+        }
+    }
+}
